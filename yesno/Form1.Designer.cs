ac5047c00bf0e4f96fb390ff4014f6c273d3b20a
unknown <unknown@example.com>
2019-05-31 16:49:52 +0900

    1

---
+++ yesno/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace yesno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(10, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(464, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yes or No?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::yesno.Properties.Resources.directory_973992_640;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(464, 408);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.PictureBox1_LoadProgressChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBar1.Location = new System.Drawing.Point(10, 10);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(464, 10);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "yesno.wtf";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ yesno/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Codeplex.Data;
+using System;
+using System.ComponentModel;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace yesno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Answer { get; set; }
+        public string Image { get; set; }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            progressBar1.Value = 0;
+
+            var json = "";
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows XP)");
+                webClient.Encoding = Encoding.UTF8;
+                json = webClient.DownloadString("https://yesno.wtf/api");
+            }
+
+            var obj = DynamicJson.Parse(json);
+
+            Answer = obj.answer;
+            Image = obj.image;
+
+            //非同期的に画像を読み込んで表示する
+            pictureBox1.WaitOnLoad = false;
+            pictureBox1.LoadAsync(Image);
+
+            //pictureBox1.ImageLocation = obj.image;//同期表示版
+        }
+
+        private void PictureBox1_LoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+            if (e.ProgressPercentage == 100)
+            {
+                MessageBox.Show(Answer.ToUpper());
+            }
+        }
+    }
+}
